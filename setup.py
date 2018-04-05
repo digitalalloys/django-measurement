f9e2e6a77820fec947474aa9cad5bee8a383953e
Matthew Downey <matthew.downey@webit.com.au>
2018-04-05 14:10:35 +1000

    Changing the setup so can `pip install -e`

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
-#!/usr/bin/env python
-from setuptools import setup
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from distutils.core import setup
+
 
 setup(
-    setup_requires=['pbr'],
-    pbr=True,
+    name='django-measurement',
+    version='0.1',
+    url='https://github.com/webit-mdowney/django-measurement',
+    packages=[
+        'django_measurement',
+    ],
+    install_requires=[
+        'measurement',
+    ],
 )
